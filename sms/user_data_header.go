897845e0923bcd90499cd2518ee3e7fdac49d6e6
sisoft <sisoftrg@gmail.com>
2022-11-08 11:07:34 +0300

    Fix decoding of UDH for concatenated messages with 16-bit reference tag. Also, skip UDH with unknown IEI (such as media/settings download).

--- sms/user_data_header.go
+++ sms/user_data_header.go
@@ -14,9 +14,15 @@ func (udh *UserDataHeader) ReadFrom(octets []byte) error {
 	}
 
 	h := octets[:headerLng]
-	udh.Sequence = int(h[5])
-	udh.TotalNumber = int(h[4])
-	udh.Tag = int(h[3])
+	if int(h[1]) == 0 && headerLng == 6 {
+		udh.Sequence = int(h[5])
+		udh.TotalNumber = int(h[4])
+		udh.Tag = int(h[3])
+	} else if int(h[1]) == 8 && headerLng == 7 {
+		udh.Sequence = int(h[6])
+		udh.TotalNumber = int(h[5])
+		udh.Tag = int(h[3])*256 + int(h[4])
+	}
 
 	return nil
 }
